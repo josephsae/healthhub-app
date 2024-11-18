c61763c85aec0216bf858ed3ece4a730dea2b0b2
Joseph Brayan Smith Caicedo Sáenz <joseph.caice08@gmail.com>
2024-11-18 01:04:25 -0500

    Revert "configure test report"
    This reverts commit a59b977ed332b64cbc7b7922a94a92b6acf8d388.

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,20 +1,11 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  setupFiles: ["dotenv/config"],
-  moduleFileExtensions: ["js", "ts"],
-  testMatch: ["**/tests/**/*.test.ts"],
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-  },
-  reporters: [
-    "default",
-    [
-      "jest-junit",
-      {
-        outputDirectory: "./reports/junit",
-        outputName: "junit.xml",
-      },
-    ],
-  ],
-};
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    setupFiles: ['dotenv/config'],
+    moduleFileExtensions: ['js', 'ts'],
+    testMatch: ['**/tests/**/*.test.ts'],
+    transform: {
+      '^.+\\.ts$': 'ts-jest',
+    },
+  };
+  
