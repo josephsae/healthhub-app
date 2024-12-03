a7ed45ffd1777ad47f283f6b5eeb5a4d23f05f73
Joseph Brayan Smith Caicedo Sáenz <joseph.caice08@gmail.com>
2024-12-03 17:41:41 -0500

    integrate github actions

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverage: true, 
+    coverageDirectory: 'coverage', 
+    coverageReporters: ['text', 'lcov', 'html'], 
+    collectCoverageFrom: [
+      'src/**/*.{js,ts}', 
+      '!src/**/*.d.ts',  
+      '!src/index.ts',   
+    ],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'], 
     setupFiles: ['dotenv/config'],
     moduleFileExtensions: ['js', 'ts'],
     testMatch: ['**/tests/**/*.test.ts'],
